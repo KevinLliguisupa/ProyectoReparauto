153d84ccf0d849142fe1c5b348034f12948f4b5b
Kevin Vinueza <kxvinuezac@utn.edu.ec>
2021-12-29 01:00:07 -0500

    Creacion de las vistas, controlador y manager para el jefe de proyectos, todo esta en blanco, solo esta definido los nombres de los archivos

---
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/inventario/managers/ManagerJefeTaller.java
@@ -0,0 +1,20 @@
+package minimarketdemo.model.inventario.managers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerJefeTaller
+ */
+@Stateless
+@LocalBean
+public class ManagerJefeTaller {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerJefeTaller() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ minimarketdemoWeb/src/minimarketdemo/controller/inventario/BeanJefeTaller.java
@@ -0,0 +1,16 @@
+package minimarketdemo.controller.inventario;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class BeanJefeTaller implements Serializable {
+
+	public BeanJefeTaller() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
