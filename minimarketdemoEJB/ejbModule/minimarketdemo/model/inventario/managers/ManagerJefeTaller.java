a39e71917f232734ac1bfd8fd24cc0235bc1f1a8
Kevin Vinueza <kxvinuezac@utn.edu.ec>
2021-12-29 01:43:46 -0500

    metodos y atributos del controlador y manager para el jefe de taller, los metodos estan vacios

--- minimarketdemoEJB/ejbModule/minimarketdemo/model/inventario/managers/ManagerJefeTaller.java
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/inventario/managers/ManagerJefeTaller.java
@@ -1,7 +1,15 @@
 package minimarketdemo.model.inventario.managers;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import minimarketdemo.model.core.entities.InvMaterial;
+import minimarketdemo.model.core.entities.InvProveedor;
+import minimarketdemo.model.core.entities.RecVehiculo;
+import minimarketdemo.model.core.entities.ThmEmpleado;
+import minimarketdemo.model.core.managers.ManagerDAO;
 
 /**
  * Session Bean implementation class ManagerJefeTaller
@@ -10,11 +18,60 @@ import javax.ejb.Stateless;
 @LocalBean
 public class ManagerJefeTaller {
 
-    /**
-     * Default constructor. 
-     */
+    private ManagerDAO mDao;
     public ManagerJefeTaller() {
-        // TODO Auto-generated constructor stub
+       
     }
+    
+    public List<InvMaterial> findAllMaterial(){
+    	return mDao.findAll(InvMaterial.class);
+    }
+    
+    public void deleteMaterial(InvMaterial material) {
+    	
+    }
+    public void retirarMaterial(InvMaterial material,int cantidad, int vehiculo_id, int empleado_id) {
+    	
+    }
+    public void ingresarMaterial(InvMaterial material,InvProveedor proveedor) {
+    	
+    }
+    
+    public void calcularSock(InvMaterial material,int cantidad, boolean ingreso) {
+    	
+    }
+    
+    public void updateMaterial(InvMaterial material) {
+    	
+    }
+    
+    public InvMaterial findMaterialId(int id) {
+    	
+    }
+    public List<InvTipo> findAllTipoMaterial() {
+    	
+    }
+    public InvTipo findTipoMaterialById(int id) {
+    	
+    }
+    public InvProveedor findIdProveedor(int id) {
+    	
+    }
+    public List<InvProveedor> findAllProveedor() {
+    	
+    }
+    public  List<RecVehiculo> findAllVehiculos(){
+    	
+    }
+    public RecVehiculo findVehiculosById(int id) {
+    	
+    }
+    public List<ThmEmpleado>findAllEmpleados() {
+    	
+    }
+    public ThmEmpleado findEmpleadosById(int id) {
+    	
+    }
+    
 
 }
--- minimarketdemoWeb/src/minimarketdemo/controller/inventario/BeanJefeTaller.java
+++ minimarketdemoWeb/src/minimarketdemo/controller/inventario/BeanJefeTaller.java
@@ -1,16 +1,73 @@
 package minimarketdemo.controller.inventario;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+
+import minimarketdemo.model.core.entities.InvMaterial;
+import minimarketdemo.model.core.entities.InvProveedor;
+import minimarketdemo.model.core.entities.InvTipo;
+import minimarketdemo.model.core.entities.RecVehiculo;
+import minimarketdemo.model.core.entities.ThmEmpleado;
+import minimarketdemo.model.inventario.managers.ManagerJefeTaller;
 
 @Named
 @SessionScoped
 public class BeanJefeTaller implements Serializable {
 
+	@EJB
+	private ManagerJefeTaller mJefeTaller;
+	private List<InvMaterial> listaMateriales;
+	private int idMaterial;
+	private InvMaterial nuevoMaterial;
+	private InvMaterial material;
+	private List<ThmEmpleado> listaEmpleados;
+	private int id_empleados;
+	private ThmEmpleado empleados;
+	private List<InvProveedor> listaProveedor;
+	private int idProveedor;
+	private InvProveedor proveedor;
+	private List<RecVehiculo> listaVehiculos;
+	private int id_vehiculos;
+	private RecVehiculo vehiculos;
+	private List<InvTipo> listaTipo;
+	private int idTipo;
+	private InvTipo nuevoTipo;
+	private InvTipo tipo;
+	private int cantidadRetirar;
+	
+	
 	public BeanJefeTaller() {
-		// TODO Auto-generated constructor stub
+		
 	}
-
+	@PostConstruct
+	public void inicializar() {
+		
+	}
+	
+	public void actionDeleteMaterial() {
+		
+	}
+	public void actionIngresarMaterial() {
+		
+	}
+	public void actionUpdateMaterial() {
+		
+	}
+	public void actionRetirarMaterial() {
+		
+	}
+	public void actionCreateTipoMaterial() {
+		
+	}
+	public void actionDeleteTipoMaterial() {
+		
+	}
+	public void actionUpdateTipoMaterial() {
+		
+	}
 }
