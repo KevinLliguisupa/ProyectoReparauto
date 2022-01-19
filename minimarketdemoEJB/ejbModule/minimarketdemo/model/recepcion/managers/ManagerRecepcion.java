b18e87d6303043692060d1855794b547232036c6
Kevin Lliguisupa <kevinlliguisupa6@gmail.com>
2022-01-19 09:01:17 -0500

    Ingreso de recepciones

---
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/recepcion/managers/ManagerRecepcion.java
@@ -0,0 +1,139 @@
+package minimarketdemo.model.recepcion.managers;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import minimarketdemo.model.auditoria.managers.ManagerAuditoria;
+import minimarketdemo.model.core.entities.InvMaterial;
+import minimarketdemo.model.core.entities.InvSalida;
+import minimarketdemo.model.core.entities.RecCliente;
+import minimarketdemo.model.core.entities.RecRecepcionCabecera;
+import minimarketdemo.model.core.entities.RecRecepcionDetalle;
+import minimarketdemo.model.core.entities.RecServicio;
+import minimarketdemo.model.core.entities.RecVehiculo;
+import minimarketdemo.model.core.entities.SegUsuario;
+import minimarketdemo.model.core.entities.ThmEmpleado;
+import minimarketdemo.model.core.managers.ManagerDAO;
+import minimarketdemo.model.seguridades.dtos.LoginDTO;
+
+/**
+ * Session Bean implementation class ManagerRecepcion
+ */
+@Stateless
+@LocalBean
+public class ManagerRecepcion {
+
+	@EJB
+	private ManagerDAO mDao;
+
+	@EJB
+	private ManagerAuditoria mAuditoria;
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public ManagerRecepcion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<RecServicio> findAllServicios() {
+		return mDao.findWhere(RecServicio.class, "rec_ser_estado=true", "rec_ser_nombre ASC");
+	}
+
+	public List<RecCliente> findAllClientes() {
+		return mDao.findWhere(RecCliente.class, "cli_estado=true", "cli_nombre_apellido ASC");
+	}
+
+	public List<RecVehiculo> findAllVehiculos() {
+		return mDao.findWhere(RecVehiculo.class, "veh_estado=true", "veh_placa ASC");
+	}
+
+	public int findNumeroRecepcion() throws Exception {
+		// return mDao.obtenerValorMax(RecRecepcionCabecera.class, "recCabId");
+
+		Query q;
+		String sentenciaSQL;
+		int valorMax;
+		try {
+			sentenciaSQL = "SELECT MAX(o.recCabId) FROM RecRecepcionCabecera o";
+			q = em.createQuery(sentenciaSQL);
+			valorMax = (Integer) q.getSingleResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Error al obtener valor maximo"+ e.getMessage());
+		}
+		return valorMax;
+	}
+	
+	public BigDecimal calcularTotal(List<RecServicio> serviciosCotizacion) {
+		BigDecimal total= new BigDecimal(0);
+		for(RecServicio servicio : serviciosCotizacion) {
+			total= total.add(servicio.getRecSerPrecio());
+		}
+		return total;
+	}
+	
+	public List<RecServicio> retirarServicio(List<RecServicio> serviciosCotizacion, RecServicio servicioSeleccionado){
+		List<RecServicio> lista = new ArrayList<RecServicio>();
+		for (int i = 0; i < serviciosCotizacion.size(); i++) {
+			if(serviciosCotizacion.get(i).getRecSerId()!=servicioSeleccionado.getRecSerId()) {
+				lista.add(serviciosCotizacion.get(i));
+			}	
+			
+		}
+		return lista;
+	}
+	
+	public RecServicio findServicioById(int id) throws Exception {
+		return (RecServicio) mDao.findById(RecServicio.class, id);
+	}
+	
+	public void ingresarRecepcion(LoginDTO loginDTO, RecRecepcionCabecera cabeceraRecepcion, List<RecServicio> serviciosCotizacion) throws Exception {
+		cabeceraRecepcion.setRecCabEstado(true);
+		cabeceraRecepcion.setRecCabTerminado(false);
+		SegUsuario usuario = new SegUsuario();
+		usuario.setIdSegUsuario(loginDTO.getIdSegUsuario());
+		cabeceraRecepcion.setSegUsuario(usuario);;
+		mDao.insertar(cabeceraRecepcion);
+		mAuditoria.mostrarLog(loginDTO, getClass(), "ingresarRecepcion", "Recepcion de vehiculo Usu. "+
+				cabeceraRecepcion.getSegUsuario().getIdSegUsuario()+" "+" Veh. "+cabeceraRecepcion.getRecVehiculo().getVehPlaca());
+		
+		for (RecServicio servicio: serviciosCotizacion) {
+			RecRecepcionDetalle detalle = new RecRecepcionDetalle();
+			detalle.setRecDetPrecioFinal(servicio.getRecSerPrecio());
+			detalle.setRecDetConcluido(false);
+			detalle.setRecDetServicioExtra(false);
+			detalle.setRecDetEstado(true);
+			detalle.setRecDetHorasEmpleadas(0);
+			detalle.setRecRecepcionCabecera(cabeceraRecepcion);
+			detalle.setRecServicio(servicio);
+			mDao.insertar(detalle);
+			mAuditoria.mostrarLog(loginDTO, getClass(), "ingresarRecepcion", "Servicio a vehiculo Ser. "+
+					detalle.getRecServicio().getRecSerId()+" "+" Rec."+detalle.getRecRecepcionCabecera().getRecCabId());
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ minimarketdemoWeb/src/minimarketdemo/controller/recepcion/BeanRecepcion.java
@@ -0,0 +1,232 @@
+package minimarketdemo.controller.recepcion;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import minimarketdemo.controller.seguridades.BeanSegLogin;
+import minimarketdemo.model.core.entities.RecCliente;
+import minimarketdemo.model.core.entities.RecRecepcionCabecera;
+import minimarketdemo.model.core.entities.RecServicio;
+import minimarketdemo.model.core.entities.RecVehiculo;
+import minimarketdemo.model.core.utils.ModelUtil;
+import minimarketdemo.model.recepcion.managers.ManagerRecepcion;
+
+@Named
+@SessionScoped
+public class BeanRecepcion implements Serializable {
+
+	@EJB
+	private ManagerRecepcion mRecepcion;
+
+	private List<RecVehiculo> listaVehiculos;
+	private List<RecServicio> listaServicios;
+	private List<RecCliente> listaClientes;
+	
+	private RecRecepcionCabecera cabeceraRecepcion;
+	private RecVehiculo vehiculo;
+	private RecCliente cliente;
+	private Date fechaRecepcion;
+	private Time horaRecepcion;
+	
+	private int numeroRecepcion;
+	private BigDecimal precioTotal;
+	private BigDecimal abono;
+	private BigDecimal saldo;
+	
+	@Inject
+	private BeanSegLogin beanSegLogin;
+	
+	public BeanRecepcion() {
+
+	}
+
+	@PostConstruct
+	public void inicializar() throws Exception {
+		listaVehiculos = mRecepcion.findAllVehiculos();
+		listaServicios = mRecepcion.findAllServicios();
+		cabeceraRecepcion= new RecRecepcionCabecera();
+		cliente=new RecCliente();
+		vehiculo=new RecVehiculo();
+		numeroRecepcion = mRecepcion.findNumeroRecepcion();
+			
+		Date hoy = new Date();
+		fechaRecepcion= hoy;
+		horaRecepcion= new java.sql.Time(hoy.getTime());
+		abono = new BigDecimal(0);
+		
+		precioTotal=mRecepcion.calcularTotal(listaServicios);
+		saldo=precioTotal.subtract(abono);
+		cabeceraRecepcion.setRecCabFechaRecepcion(hoy);
+		cabeceraRecepcion.setRecCabFechaEntrega(ModelUtil.addDays(hoy, +3));
+		
+	}
+	
+	public void actionListenerCargarClientes() {
+		listaClientes = mRecepcion.findAllClientes();
+	}
+	
+	public void actionListenerCargarVehiculos() {
+		listaVehiculos = mRecepcion.findAllVehiculos();
+	}
+	
+	public void actionListenerRetirarServicio(int servicioId) throws Exception {
+		RecServicio servicioSeleccionado=mRecepcion.findServicioById(servicioId);
+		listaServicios=mRecepcion.retirarServicio(listaServicios, servicioSeleccionado);
+		precioTotal=mRecepcion.calcularTotal(listaServicios);
+		saldo=precioTotal.subtract(abono);
+	}
+	
+	public void actionListenerCalcularSaldo() {
+		precioTotal=mRecepcion.calcularTotal(listaServicios);
+		saldo=precioTotal.subtract(abono);	
+	}
+	
+	public void actionSeleccionarCliente(RecCliente clienteSeleccionado) {
+		cliente=clienteSeleccionado;
+	}
+	
+	public void actionSeleccionarVehiculo(RecVehiculo vehiculoSeleccionado) {
+		vehiculo=vehiculoSeleccionado;
+	}
+	
+	public void actionListenerCrearRecepcion() throws Exception {
+		cabeceraRecepcion.setRecCabAbono(abono);
+		cabeceraRecepcion.setRecCabFechaRecepcion(fechaRecepcion);
+		cabeceraRecepcion.setRecCabTotal(precioTotal);
+		cabeceraRecepcion.setRecCabHora(horaRecepcion);
+		cabeceraRecepcion.setRecCabSaldo(saldo);
+		cabeceraRecepcion.setRecVehiculo(vehiculo);
+		cabeceraRecepcion.setRecCliente(cliente);
+		mRecepcion.ingresarRecepcion(beanSegLogin.getLoginDTO(),cabeceraRecepcion, listaServicios);
+		cabeceraRecepcion=new RecRecepcionCabecera();
+		inicializar();
+		listaServicios=new ArrayList<RecServicio>();
+		precioTotal=mRecepcion.calcularTotal(listaServicios);
+		saldo=precioTotal.subtract(abono);
+		
+		
+	}
+	
+	
+	public List<RecServicio> getListaServicios() {
+		return listaServicios;
+	}
+
+	public void setListaServicios(List<RecServicio> listaServicios) {
+		this.listaServicios = listaServicios;
+	}
+
+	public RecVehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(RecVehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	public RecCliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(RecCliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<RecCliente> getListaClientes() {
+		return listaClientes;
+	}
+
+	public void setListaClientes(List<RecCliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
+	public List<RecVehiculo> getListaVehiculos() {
+		return listaVehiculos;
+	}
+
+	public void setListaVehiculos(List<RecVehiculo> listaVehiculos) {
+		this.listaVehiculos = listaVehiculos;
+	}
+
+	public int getNumeroRecepcion() {
+		return numeroRecepcion;
+	}
+
+	public void setNumeroRecepcion(int numeroRecepcion) {
+		this.numeroRecepcion = numeroRecepcion;
+	}
+
+	public Time getHoraRecepcion() {
+		return horaRecepcion;
+	}
+
+	public void setHoraRecepcion(Time horaRecepcion) {
+		this.horaRecepcion = horaRecepcion;
+	}
+
+	public Date getFechaRecepcion() {
+		return fechaRecepcion;
+	}
+
+	public void setFechaRecepcion(Date fechaRecepcion) {
+		this.fechaRecepcion = fechaRecepcion;
+	}
+	
+	public BigDecimal getAbono() {
+		return abono;
+	}
+
+	public void setAbono(BigDecimal abono) {
+		this.abono = abono;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getFechaActualTexto() {
+		Calendar hoy = Calendar.getInstance();
+		
+		String dia=""+hoy.get(Calendar.MONTH)+1;
+
+		
+		String fechatexto=hoy.get(Calendar.YEAR)+"/"+dia+"/"+hoy.get(Calendar.DAY_OF_MONTH);
+		return fechatexto;
+	}
+
+	public BigDecimal getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(BigDecimal precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	public RecRecepcionCabecera getCabeceraRecepcion() {
+		return cabeceraRecepcion;
+	}
+
+	public void setCabeceraRecepcion(RecRecepcionCabecera cabeceraRecepcion) {
+		this.cabeceraRecepcion = cabeceraRecepcion;
+	}
+
+	
+	
+	
+	
+}
